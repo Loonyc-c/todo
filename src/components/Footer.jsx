249ae61884e70e8cff9c59097c832add2d5ed0af
Battulga <battulgabathuyg54@gmail.com>
2025-01-10 22:25:37 +0800

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,17 +12,33 @@ const Footer = (props) => {
   };
 
   const clearCompletedTasks = () => {
-    const activeTasks = tasks.filter((task) => task.status !== "COMPLETED");
-    setTasks(activeTasks);
+    const updatedTasks = tasks.map((task) => {
+      if (task.status === "COMPLETED") {
+        return {
+          ...task,
+          status: "DELETED",
+          logs: [...task.logs, { action: "Deleted", time: new Date().toLocaleString() }],
+        };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   };
 
   return (
     <>
       {filter !== "LOGS" && (
         <div className="taskStats">
+          <div className="taskStat">
           <p>{getTaskStatus()}</p>
           <Button text="Clear Completed" onClick={clearCompletedTasks} />
+          </div>
+        <div className="origin"> 
+          <p>Powered by</p>
+          <a href="https://pinecone.mn/"> Pinecone academy </a>
         </div>
+        </div>
+
       )}
     </>
   );
